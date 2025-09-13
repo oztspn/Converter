73e0b1130b9611ba11f7c71f8bd436603b5fe58a
ozts <orysiia@gmail.com>
2025-09-13 23:09:38 +0300

    Fix description

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,6 @@ package app;
 public class Main {
     private static final double CONV_K = 2.20462;
     public static void main(String[] args) {
-        System.out.println("Converter App");
         System.out.println("App for measures converting.");
 
         double kgs = 5;
